6f90a6140601a75ba0286fcc126f932517730049
Mohamed Aymane Farhi <mfarhi@e1r5p3.1337.ma>
2021-04-10 19:07:45 +0100

    Finished training function

--- train.py
+++ train.py
@@ -1,11 +1,11 @@
 import csv
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import MinMaxScaler
 
 
 def		train(mileage, price, learning_rate, n_epochs):
 	m = float(len(mileage))
-	print("m:", m)
 	theta0, theta1 = 0, 0
 	for _ in range(n_epochs):
 		price_estimate = mileage * theta1 + theta0
@@ -17,7 +17,7 @@ def		train(mileage, price, learning_rate, n_epochs):
 		tmp_theta1 = np.sum(product)*learning_rate/m
 		theta0 -= tmp_theta0
 		theta1 -= tmp_theta1
-		print(theta0, theta1)
+		# print(theta0, theta1)
 	return theta0, theta1
 
 
@@ -38,9 +38,33 @@ def		main():
 	print(count - 1)
 	# plt.scatter(mileage, price)
 	# plt.show()
-	learning_rate = 0.00000000001
-	n_epochs = 1000
-	theta0, theta1 = train(mileage, price, learning_rate, n_epochs)
+
+	# Data normalization
+	mileage_scaler = MinMaxScaler()
+	mileage = mileage.reshape(-1, 1)
+	mileage_norm = mileage_scaler.fit_transform(mileage)
+	# print(mileage_norm)
+
+	price_scaler = MinMaxScaler()
+	price = price.reshape(-1, 1)
+	price_norm = price_scaler.fit_transform(price)
+	# print(price_norm)
+
+	learning_rate = 0.01
+	n_epochs = 10000
+	theta0_norm, theta1_norm = train(mileage_norm, price_norm, learning_rate, n_epochs)
+	# print(theta0_norm, theta1_norm)
+
+	predicted_price_norm = theta1_norm * mileage_norm + theta0_norm
+	predicted_price = price_scaler.inverse_transform(predicted_price_norm)
+	# print(predicted_price)
+	plt.scatter(mileage, price)
+	plt.plot(mileage, predicted_price, color='red')
+	plt.show()
+
+	# Calculating real values of theta0 and theta1
+	theta0 = (theta0_norm - (theta1_norm * mileage.min()) / (mileage.max() - mileage.min())) * (price.max() - price.min()) + price.min()
+	theta1 = (theta1_norm / (mileage.max() - mileage.min())) * (price.max() - price.min())
 	print(theta0, theta1)
 
 
